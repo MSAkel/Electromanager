92818b2e9d4c84d1920d9e2ddaa47c57dc78f9f2
MSAkel <akel.ms93@gmail.com>
2018-01-09 20:45:07 +0300

    predefined list workflow

--- src/pages/add-device/display-cat/display-cat.ts
+++ src/pages/add-device/display-cat/display-cat.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ModalController } from 'ionic-angular';
 
-
+import { AddModalPage } from "./add-modal/add-modal";
 import { CatDevice } from "../../../data/device-cat.interface";
-import { CreatePage } from "../create/create";
-
-import {DeviceListService} from '../../../services/devices-list';
+//import { CreatePage } from "../create/create";
+//import {DeviceListService} from '../../../services/devices-list';
 
 @IonicPage()
 @Component({
@@ -15,16 +14,17 @@ import {DeviceListService} from '../../../services/devices-list';
 export class DisplayCatPage implements OnInit{
   deviceGroup: {category: string, devices: CatDevice[], icon: string};
   //index: number;
-  name: string;
-  quantity: number;
-  power: number;
-  hours: number;
-  totalHours: number;
+  // name: string;
+  // quantity: number;
+  // power: number;
+  // hours: number;
+  // totalHours: number;
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     private alertCtrl: AlertController,
-     private dlService: DeviceListService) {
+     //private alertCtrl: AlertController,
+     //private dlService: DeviceListService,
+     private modalCtrl: ModalController) {
   }
 
   ngOnInit() {
@@ -32,32 +32,33 @@ export class DisplayCatPage implements OnInit{
   }
 
   onAddDevice(selectedDevice: CatDevice) {
-
     console.log('test');
+    const modal = this.modalCtrl.create(AddModalPage, selectedDevice);
+    modal.present();
 
-    const alert = this.alertCtrl.create({
-      title: 'Add Device',
-      //subTitle: 'Are you sure?',
-      message: 'Add item to your list?',
-      buttons: [
-        {
-          text: "Confirm",
-          handler: () => {
-            //this.navCtrl.push(CreatePage, {mode: 'Edit', device: this.deviceCat, index: this.index});
-            this.dlService.addDevice(selectedDevice.name, selectedDevice.quantity, selectedDevice.power, selectedDevice.hours, selectedDevice.daysUsed);
-          }
-        },
-        {
-          text: "Cancel",
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancelled');
-          }
-        }
-      ]
-    });
-
-    alert.present();
+    // const alert = this.alertCtrl.create({
+    //   title: 'Add Device',
+    //   //subTitle: 'Are you sure?',
+    //   message: 'Add item to your list?',
+    //   buttons: [
+    //     {
+    //       text: "Confirm",
+    //       handler: () => {
+    //         //this.navCtrl.push(CreatePage, {mode: 'Edit', device: this.deviceCat, index: this.index});
+    //         this.dlService.addDevice(selectedDevice.name, selectedDevice.quantity, selectedDevice.power, selectedDevice.hours, selectedDevice.daysUsed);
+    //       }
+    //     },
+    //     {
+    //       text: "Cancel",
+    //       role: 'cancel',
+    //       handler: () => {
+    //         console.log('Cancelled');
+    //       }
+    //     }
+    //   ]
+    // });
+    //
+    // alert.present();
   }
 
 }
